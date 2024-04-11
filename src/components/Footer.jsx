750537f7dfa83d8e73f97bb3ea4fc630a688b78c
Adhil Rumais <adhilrumais007@gmail.com>
2024-04-12 01:34:18 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
   return (
     <div className="text-white w-full mx-auto py-16 max-w-[1240px] grid lg:grid-cols-3 gap-8">
         <div>
+          <a href="#navbar">
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        </a>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit saepe iusto officia illum perferendis atque. Beatae sint illo consequatur maiores. Ipsum, veritatis dolorum possimus laborum rerum vitae! Porro, quis sequi.</p>
         <div className='flex gap-8'>
             <FaFacebook size={30}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     setnav(!nav)
   }
   return (
-    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto">
+    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto" id="navbar">
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
